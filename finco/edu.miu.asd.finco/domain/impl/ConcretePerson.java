ecca773d2241cd3aaa41b79d6ad9ae6616133256
Md Shajedul Islam <j.shajid@gmail.com>
2022-07-13 08:52:18 -0500

    person to organization relationship

--- finco/edu.miu.asd.finco/domain/impl/ConcretePerson.java
+++ finco/edu.miu.asd.finco/domain/impl/ConcretePerson.java
@@ -1,11 +1,21 @@
 package domain.impl;
 
+import domain.IOrganization;
 import domain.IPerson;
 
 import java.io.Serializable;
 import java.sql.Date;
 
 public class ConcretePerson extends Customer implements IPerson, Serializable {
+    private IOrganization organization;
+
+    public IOrganization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(IOrganization organization) {
+        this.organization = organization;
+    }
 
     public ConcretePerson(String nm, String ste, String ct, String sta, String zp, String em) {
         super(nm, ste, ct, sta, zp, em);
