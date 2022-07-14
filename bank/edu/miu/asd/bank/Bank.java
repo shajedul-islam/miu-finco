5eb448e4bd049fa54ef5b257c1cc334d778becb0
khandokarsabbir@gmail.com <Gh02201018>
2022-07-13 21:57:58 -0500

    Added GUI to main

--- bank/edu/miu/asd/bank/Bank.java
+++ bank/edu/miu/asd/bank/Bank.java
@@ -6,17 +6,38 @@ import project.bank.edu.miu.asd.bank.domain.BankAccount;
 import project.bank.edu.miu.asd.bank.domain.impl.Checkings;
 import project.bank.edu.miu.asd.bank.factory.AccountFactory;
 import project.bank.edu.miu.asd.bank.factory.AccountType;
+import project.bank.edu.miu.asd.bank.gui.BankFrm;
+
+import javax.swing.*;
 
 public class Bank {
     public static void main(String[] args) {
         IOCContainer.loadComponents(Bank.class.getResourceAsStream("bank-dependencies.properties"));
 
-        BankAccountController bankAccountController = (BankAccountController) IOCContainer.getComponent("bankAccountController");
+        //BankAccountController bankAccountController = (BankAccountController) IOCContainer.getComponent("bankAccountController");
 
-        BankAccount bankAccount = AccountFactory.CreateAccount(AccountType.SAVINGS, "12", "person", 6);
+        //BankAccount bankAccount = AccountFactory.CreateAccount(AccountType.SAVINGS, "12", "person", 6);
 
-        bankAccountController.CreateAccount(bankAccount);
+        //bankAccountController.CreateAccount(bankAccount);
 
-        System.out.println();
+        //System.out.println();
+
+        try {
+	    // Add the following code if you want the Look and Feel
+		    // to be set to the Look and Feel of the native system.
+
+	    try {
+		        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		    }
+		    catch (Exception e) {
+	    }
+         //Create a new instance of our application's frame, and make it visible.
+		(new BankFrm()).setVisible(true);
+		}
+		catch (Throwable t) {
+			t.printStackTrace();
+			//Ensure the application exits with an error condition.
+			System.exit(1);
+		}
     }
 }
